5168013fdc20b155bad355cd5782c3161037cee4
c0np4nn4 <c0np4nn4@gmail.com>
2022-03-09 14:22:11 +0900

    #1 GNB 버튼 색상 수정, 검색창 만드는 중

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,49 @@
 import styles from "./SearchBar.module.css";
+import { useState } from "react";
 
 function SearchBar() {
+  const [searchingWord, setSearchingWord] = useState("");
+  const [searching, setSearching] = useState(false);
+
+  const onchange = (event) => {
+    setSearchingWord(event.target.value);
+  };
+
+  const SearchPreview = ({ keyword }) => {
+    return (
+      <div className={styles.searchResults}>
+        <div>{keyword}</div>
+      </div>
+    );
+  };
+
+  const isSearching = () => {
+    setSearching(true);
+  };
+
+  const stopSearching = () => {
+    setSearching(false);
+  };
+
   return (
     <div>
-      <img
-        className={styles.icon}
-        src="https://user-images.githubusercontent.com/49471288/156142084-1cd52e4a-db14-4ced-93b8-2d23170f0291.png"
-      />
+      {searchingWord.length > 0 ? (
+        <SearchPreview keyword={searchingWord} />
+      ) : null}
       <input
-        className={styles.searchBar}
+        className={
+          searching || searchingWord.length > 0
+            ? styles.searchBarFocus
+            : styles.searchBar
+        }
         type="search"
         id="search"
         placeholder="Search Nickname and Address"
+        onChange={onchange}
+        onFocus={isSearching}
+        onBlur={stopSearching}
       />
+      <img className={styles.icon} src="img/searchIcon.png" alt="searchIcon" />
     </div>
   );
 }
