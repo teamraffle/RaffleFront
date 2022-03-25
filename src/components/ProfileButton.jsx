e9c9bf157450b72884dd8d6f728c1572377f985e
c0np4nn4 <c0np4nn4@gmail.com>
2022-03-25 13:39:44 +0900

    #8 profile에 마우스 호버링 시 틀 만듦

--- src/components/ProfileButton.jsx
+++ src/components/ProfileButton.jsx
@@ -1,8 +1,45 @@
 import styles from "./ProfileButton.module.css";
 import { useState } from "react";
 
+const ProfileMiniBox = () => {
+  return (
+    <div className={styles.profileMiniBox}>
+      {/* nick name */}
+      <div> nikcname </div>
+      {/* wallet address */}
+      <div> wallet address</div>
+      {/* grey split line */}
+      <div> ---------- </div>
+      {/* Button Portfolio */}
+      <div>button</div>
+      {/* Button Switch Wallet */}
+      <div>switch wallet</div>
+      {/* Dark Toggle switch */}
+      <div>dark mode</div>
+      {/* Sign out */}
+      <div>Sign Out</div>
+    </div>
+  );
+};
 const ProfileButton = () => {
-  return <button className={styles.profileButton}></button>;
+  const [mouseEnter, setMouseEnter] = useState(false);
+
+  return (
+    <div>
+      <div
+        className={styles.profileButton}
+        onMouseOver={() => {
+          setMouseEnter(true);
+        }}
+        onMouseLeave={() => {
+          setMouseEnter(false);
+        }}
+      >
+        a
+      </div>
+      {mouseEnter ? <ProfileMiniBox /> : null}
+    </div>
+  );
 };
 
 export default ProfileButton;
